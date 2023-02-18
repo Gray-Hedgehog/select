78da1dc4123e8c62dd3b84bbddef35875383ca4f
Aslan Abaev <hedgehog5349@gmail.com>
2023-02-18 17:30:06 +0300

    dev select

--- src/blocks/select-1/select-1.js
+++ src/blocks/select-1/select-1.js
@@ -1,36 +1,46 @@
 o2.select = {
-	select: document.querySelector('.select-1'),
-	selectInput: document.querySelector('.select-1__input'),
-	selectInputHidden: document.querySelector('._selected-value'),
-	selectItems: document.querySelector('.select-1__items'),
-	selectIcon:  document.querySelector('.select-1__icon'),
+	// select: undefined,
+	// selectInput: document.querySelector('.select-1__input'),
+	// selectInputHidden: document.querySelector('._selected-value'),
+	// selectItems: null,
+	// selectIcon:  null,
 
-	handleClose(event) {
-		this.selectItems.classList.remove('_active-items');
-		this.selectIcon.classList.remove('select-1__icon--open');
+	handleClose(selectItems, selectIcon) {
+		selectItems.classList.remove('_active-items');
+		selectIcon.classList.remove('select-1__icon--open');
 	},
 
-	windowClose(event) {
-		const hundleClick = event.composedPath().includes(this.select);
-
-		if ( hundleClick == false ) {
-			this.selectItems.classList.remove('_active-items');
-			this.selectIcon.classList.remove('select-1__icon--open');
+	windowClose(event, select) {
+		// // let boolSelect = event.target.closest('.select-1').querySelector('');
+		// console.log(event, selectItems);
+		const hundleClick = event.composedPath().includes(select);
+		console.log(select);
+		if (  ) {
+			selectItems.classList.remove('_active-items');
+			selectIcon.classList.remove('select-1__icon--open');
 		}
 	},
 
-	handleToggle() {
-		this.selectItems.classList.toggle('_active-items');
-		this.selectIcon.classList.toggle('select-1__icon--open');
+	handleToggle(event) {
+		// select = event.target.;
+		// console.log(select);
+		let select = event.closest('.select-1');
+		let selectItems = event.target.closest('.select-1').querySelector('.select-1__items');
+		// console.log(selectItems);
+		let selectIcon  = event.target.closest('.select-1').querySelector('.select-1__icon');
+		this.windowClose(selectItems, select);
+		selectItems.classList.toggle('_active-items');
+		selectIcon.classList.toggle('select-1__icon--open');
 	},
 
 	handleChoose(event) {
+		let selectInput = event.target.closest('.select-1').querySelector('.select-1__input');
+		let selectInputHidden = event.target.closest('form').querySelector('._selected-value');
 		let text = event.target.innerText;
-		this.handleClose();
-		this.selectInput.value = text;
-		this.selectInputHidden.value = text;
-		console.log(this.selectInputHidden.value);
+		this.handleClose(selectItems, selectIcon);
+		selectInput.value = text;
+		selectInputHidden.value = text;
 	}
 };
 
-window.addEventListener('click', event => o2.select.windowClose(event));
+window.addEventListener('click', event => o2.select.windowClose(event, o2.select.selectItems));
