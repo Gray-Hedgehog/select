9f030d6b864db5d2ba1d3a6dd416a3364a58584c
Aslan Abaev <hedgehog5349@gmail.com>
2023-02-20 17:33:32 +0300

    fix select-1

--- src/blocks/select-1/select-1.js
+++ src/blocks/select-1/select-1.js
@@ -1,46 +1,54 @@
 o2.select = {
-	// select: undefined,
-	// selectInput: document.querySelector('.select-1__input'),
-	// selectInputHidden: document.querySelector('._selected-value'),
-	// selectItems: null,
-	// selectIcon:  null,
+	handleToggle(event) {
+		let selectItems = event.target.closest('.select-1').querySelector('.select-1__items');
+		let selectIcon  = event.target.closest('.select-1').querySelector('.select-1__icon');
+		selectItems.classList.toggle('_active-items');
+		selectIcon.classList.toggle('select-1__icon--open');
+	},
+
+	handleChoose(event) {
+		let selectItems = event.target.closest('.select-1').querySelector('.select-1__items');
+		let selectIcon  = event.target.closest('.select-1').querySelector('.select-1__icon');
+		let selectInput = event.target.closest('.select-1').querySelector('.select-1__input');
+		let selectInputHidden = event.target.closest('form').querySelector('._selected-value');
+		let text = event.target.innerText;
+		this.handleClose(selectItems, selectIcon);
+
+		selectInput.value = text;
+		selectInputHidden.value = text;
+	},
 
 	handleClose(selectItems, selectIcon) {
 		selectItems.classList.remove('_active-items');
 		selectIcon.classList.remove('select-1__icon--open');
 	},
 
-	windowClose(event, select) {
-		// // let boolSelect = event.target.closest('.select-1').querySelector('');
-		// console.log(event, selectItems);
-		const hundleClick = event.composedPath().includes(select);
-		console.log(select);
-		if (  ) {
-			selectItems.classList.remove('_active-items');
-			selectIcon.classList.remove('select-1__icon--open');
+	windowClose(event) {
+		const selectActiveItemsAll = document.querySelectorAll('._active-items');
+		const selectOpenIcon = document.querySelector('.select-1__icon--open');
+
+		const selectAll = document.querySelectorAll('.select-1');
+		const selectItemsAll = document.querySelectorAll('.select-1__items');
+
+
+
+		selectActiveItemsAll.forEach((element) => {
+			if (element.classList.contains('_active-items')) {
+				element.classList.remove('_active-items');
+			}
+		});
+
+		console.log(event.composedPath())
+
+		console.log(event.composedPath().includes('.select-1__header'))
+		if ( event.target.closest('select-1__header') == true ) {
+			console.log('if')
+			let selectItems = event.target.closest('.select-1').querySelector('.select-1__items');
+			let selectIcon  = event.target.closest('.select-1').querySelector('.select-1__icon');
+			selectItems.classList.toggle('_active-items');
+			selectIcon.classList.toggle('select-1__icon--open');
 		}
 	},
-
-	handleToggle(event) {
-		// select = event.target.;
-		// console.log(select);
-		let select = event.closest('.select-1');
-		let selectItems = event.target.closest('.select-1').querySelector('.select-1__items');
-		// console.log(selectItems);
-		let selectIcon  = event.target.closest('.select-1').querySelector('.select-1__icon');
-		this.windowClose(selectItems, select);
-		selectItems.classList.toggle('_active-items');
-		selectIcon.classList.toggle('select-1__icon--open');
-	},
-
-	handleChoose(event) {
-		let selectInput = event.target.closest('.select-1').querySelector('.select-1__input');
-		let selectInputHidden = event.target.closest('form').querySelector('._selected-value');
-		let text = event.target.innerText;
-		this.handleClose(selectItems, selectIcon);
-		selectInput.value = text;
-		selectInputHidden.value = text;
-	}
 };
 
-window.addEventListener('click', event => o2.select.windowClose(event, o2.select.selectItems));
+window.addEventListener('click', event => o2.select.windowClose(event));
