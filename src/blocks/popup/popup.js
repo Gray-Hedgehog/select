b4d9af52be4948d35953b6536cc6852d5fba598d
Aslan Abaev <hedgehog5349@gmail.com>
2023-02-27 11:44:37 +0300

    fix select

--- src/blocks/popup/popup.js
+++ src/blocks/popup/popup.js
@@ -1,13 +1,23 @@
 o2.popup = {
-	popupIconClose: document.querySelector('._popup-close'),
 	popupOverlay: document.querySelector('._popup-overlay'),
 	popupOpen(contentClass) {
 		let contentHtml = document.querySelector(`.${contentClass}`).innerHTML;
-		// console.log(popupHtml)
 		this.popupOverlay.innerHTML = contentHtml;
 		this.popupOverlay.classList.add('_popup-open');
 	},
 	popupClose(event) {
 		this.popupOverlay.classList.remove('_popup-open')
+	},
+
+	outsideClick(event) {
+		const openClass = document.querySelector('._popup-open');
+		const boolOpen = event.composedPath().includes(openClass);
+		const collectionLenth = event.composedPath().length;
+
+		if ((boolOpen == true) && (collectionLenth === 6)) {
+			this.popupClose(event);
+		}
 	}
 }
+
+window.addEventListener('click', (event) => { o2.popup.outsideClick(event) });
--- src/blocks/select-1/select-1.js
+++ src/blocks/select-1/select-1.js
@@ -1,17 +1,17 @@
 o2.select = {
 	handleToggle(event) {
-		let selectItems = event.target.closest('.select-1').querySelector('.select-1__items');
-		let selectIcon  = event.target.closest('.select-1').querySelector('.select-1__icon');
+		const selectItems = event.target.closest('.select-1').querySelector('.select-1__items');
+		const selectIcon  = event.target.closest('.select-1').querySelector('.select-1__icon');
 		selectItems.classList.toggle('_active-items');
 		selectIcon.classList.toggle('select-1__icon--open');
 	},
 
 	handleChoose(event) {
-		let selectItems = event.target.closest('.select-1').querySelector('.select-1__items');
-		let selectIcon  = event.target.closest('.select-1').querySelector('.select-1__icon');
-		let selectInput = event.target.closest('.select-1').querySelector('.select-1__input');
-		let selectInputHidden = event.target.closest('form').querySelector('._selected-value');
-		let text = event.target.innerText;
+		const selectItems = event.target.closest('.select-1').querySelector('.select-1__items');
+		const selectIcon  = event.target.closest('.select-1').querySelector('.select-1__icon');
+		const selectInput = event.target.closest('.select-1').querySelector('.select-1__input');
+		const selectInputHidden = event.target.closest('form').querySelector('._selected-value');
+		const text = event.target.innerText;
 		this.handleClose(selectItems, selectIcon);
 
 		selectInput.value = text;
@@ -23,61 +23,20 @@ o2.select = {
 		selectIcon.classList.remove('select-1__icon--open');
 	},
 
-	windowClose(event) {
-		const selectActiveItemsAll = document.querySelectorAll('._active-items');
-		const selectOpenIcon = document.querySelector('.select-1__icon--open');
-
+	outsideClick(event) {
 		const selectAll = document.querySelectorAll('.select-1');
-		const selectItemsAll = document.querySelectorAll('.select-1__items');
+		const openClassAll = document.querySelectorAll('._active-items');
+		const openIconAll = document.querySelectorAll('.select-1__icon--open');
 
-
-
-		selectActiveItemsAll.forEach((element) => {
-			if (element.classList.contains('_active-items')) {
-				element.classList.remove('_active-items');
-			}
-		});
-
-		console.log(event.target.closest('.select-1'));
-		// console.log(event.target.closest('.select-1').includes('select-1'));
-
-		if(event.target.closest('.select-1')) {
-			console.log('yes');
-			// this.handleToggle(event);
-			let selectItems = event.target.closest('.select-1').querySelector('.select-1__items');
-			let selectIcon  = event.target.closest('.select-1').querySelector('.select-1__icon');
-			selectItems.classList.toggle('_active-items');
-			selectIcon.classList.add('select-1__icon--open');
+		if (( event.target.closest('.select-1') !== null ) || (event.target.closest('.select-1__container') === null)){
+			selectAll.forEach( function(element) {
+				if (event.composedPath().includes(element) !== true) {
+					element.querySelector('.select-1__items').classList.remove('_active-items');
+					element.querySelector('.select-1__icon').classList.remove('select-1__icon--open');
+				}
+			});
 		}
-		else
-		{
-			console.log(event.target);
-			const selectIcon = event.target.closest('.select-1').querySelector('.select-1__icon');
-
-			selectIcon.classList.remove('select-1__icon--open');
-		}
-
-		// let select = document.querySelector('.select-1')
-		// console.log(select)
-		// if (event.target.contains(select) && event.target !== select) {
-		// 	console.log('You clicked outside the box!');
-		// } else {
-		// 	console.log('You clicked inside the box!');
-		// 	select.querySelector('.select-1__items').toggle('._active-items');
-		// }
-
-		// this.handleToggle(event)
-		// console.log(event.composedPath())
-
-		// console.log(event.composedPath().includes('select-1'))
-		// if ( event.target.closest('select-1__header') == true ) {
-		// 	console.log('if')
-		// 	let selectItems = event.target.closest('.select-1').querySelector('.select-1__items');
-		// 	let selectIcon  = event.target.closest('.select-1').querySelector('.select-1__icon');
-		// 	selectItems.classList.toggle('_active-items');
-		// 	selectIcon.classList.toggle('select-1__icon--open');
-		// }
 	},
 };
 
-// window.addEventListener('click', event => o2.select.windowClose(event));
+window.addEventListener('click', event => o2.select.outsideClick(event));
