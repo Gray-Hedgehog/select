31b9b9b9369760074d26a6abfaeeb2eebdfbd2c0
Aslan Abaev <hedgehog5349@gmail.com>
2023-03-01 17:34:45 +0300

    comments for popup

--- src/blocks/popup/popup.js
+++ src/blocks/popup/popup.js
@@ -1,34 +1,58 @@
 o2.popup = {
 	popupOverlay: document.querySelector('._popup-overlay'),
+	/**
+	 * [popupOpen - method for open popup]
+	 * @param  {[string]} contentClass [selector for searching a content]
+	 */
 	popupOpen(contentClass) {
+		/**
+		 * [body - document body]
+		 * @type {[teg]}
+		 */
 		const body = document.querySelector('body');
+		/**
+		 * [contentHtml - variable for saving a html]
+		 * @type {[object]}
+		 */
 		let contentHtml = document.querySelector(`.${contentClass}`).innerHTML;
 		body.style.overflow = 'hidden';
+		/**
+		 * [innerHTML method for accepting markup ]
+		 * @type {[object]}
+		 */
 		this.popupOverlay.innerHTML = contentHtml;
+		/**
+		 * add class for open popup
+		 */
 		this.popupOverlay.classList.add('_popup-open');
 	},
+	/**
+	 * [popupClose method to close popup]
+	 */
 	popupClose() {
 		const body = document.querySelector('body');
 		body.style.overflow = 'visible';
 		this.popupOverlay.classList.remove('_popup-open')
 	},
-
-	outsideClick(event) {
+	/**
+	 * [outsideClick - method to check outside click ]
+	 */
+	outsideClick() {
 		const openClass = document.querySelector('._popup-open');
 		const boolOpen = event.composedPath().includes(openClass);
 		const collectionLenth = event.composedPath().length;
 
 		if ((boolOpen == true) && (collectionLenth === 6)) {
-			this.popupClose(event);
+			this.popupClose();
 		}
 
 		this.setEscEvent();
 	},
 
-	setEscEvent(event)
+	setEscEvent()
 	{
-		window.onkeydown = (event) => { (event.key === 'Escape') && (this.popupClose()) };
+		window.onkeydown = () => { (event.key === 'Escape') && (this.popupClose()) };
 	},
 }
 
-window.addEventListener('click', (event) => { o2.popup.outsideClick(event) });
+window.addEventListener('click', () => { o2.popup.outsideClick(event) });
